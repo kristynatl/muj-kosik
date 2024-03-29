10b2b0d8dd5104a612cb44f9eee41b627d6c9e9d
Kristýna Tlapáková <tlapakova24k@gmail.com>
2024-03-29 22:38:41 +0100

    předělání routeru

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import './styles.css';
 
@@ -7,45 +7,24 @@ import { App } from './pages/App';
 import { ListsViewPage } from './pages/ListsViewPage';
 import { ItemsPage } from './pages/ItemsPage';
 
-// type CustomRoute = {
-//   path: string;
-//   element: React.ReactNode;
-//   children?: CustomRoute[];
-// };
-
-// type CustomRouterConfig = CustomRoute[];
-
-// const routerConfig: CustomRouterConfig = [
-//   {
-//     path: '/',
-//     element: <App />,
-//     children: [
-//       {
-//         path: '/',
-//         element: <ListsViewPage />,
-//       },
-//       {
-//         path: '/:seznam',
-//         element: <ItemsPage />,
-//       },
-//     ],
-//   },
-// ];
-
-const AppRouter = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<ListsViewPage />} />
-          <Route path=":seznam" element={<ItemsPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-};
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <ListsViewPage />,
+      },
+      {
+        path: '/:seznam',
+        element: <ItemsPage />,
+      },
+    ],
+  },
+]);
 
 const appElement = document.querySelector('#app');
 if (appElement) {
-  createRoot(appElement).render(<AppRouter />);
+  createRoot(appElement).render(<RouterProvider router={router} />);
 }
